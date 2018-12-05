63055b6711b406f329dd917b0e2f1dbfc4b76030
EDUARDO FELIZARDO CANDIDO <eduardo.candido@fatec.sp.gov.br>
2018-12-05 15:39:12 -0200

    R01

--- Linguagem C/C como programar_DEITEL_6ed/03 - Desenvolvimento da Programação Estruturada/3.13/deithel.c
+++ Linguagem C/C como programar_DEITEL_6ed/03 - Desenvolvimento da Programação Estruturada/3.13/deithel.c
@@ -1,2 +1,24 @@
 /*O que o seguinte programa imprime? 
 ttinclude <stdio.h> main() { int x = 1, total = 0, y; while (x <= 10) { y = x * x printf("%ã\n", y); total += y); ++x; } printf("O total e %d\n", total); return 0; }*/
+
+#include <stdio.h> 
+#include <stdlib.h>
+/*Inicio do código*/
+int main() 
+{ 
+    /*Declaração de varáveis*/
+    int x = 1, total = 0, y;
+
+    /*Bloco de comandos*/ 
+    while (x <= 10) 
+    { 
+        y = x * x; 
+        printf("%d\n", y); 
+        total += y; ++x; 
+    } 
+
+    /*Saída de dados*/
+    printf("O total e %d\n", total); 
+    system("pause");
+    return 0; 
+}
