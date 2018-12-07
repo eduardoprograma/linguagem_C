062168e3c9b8c1fb63998fdf08fdcab51759e234
EDUARDO FELIZARDO CANDIDO <eduardo.candido@fatec.sp.gov.br>
2018-12-07 19:07:18 -0200

    R01

--- Linguagem C/C como programar_DEITEL_6ed/03 - Desenvolvimento da Programação Estruturada/3.17/deithel.c
+++ Linguagem C/C como programar_DEITEL_6ed/03 - Desenvolvimento da Programação Estruturada/3.17/deithel.c
@@ -8,9 +8,51 @@ Entre com os litros consumidos (-1 para finalizar): 10.3
 Entre com os km percorridos: 200 
 A taxa km/litro para esse tanque foi 19.417475
 
-Entre com os litros consumidos (-1 para finalizar): 
-5 Entre com os km percorridos: 120 
+Entre com os litros consumidos (-1 para finalizar): 5 
+Entre com os km percorridos: 120 
 A taxa km/litro para esse tanque foi 24.000000
 
 Entre com os litros consumidos (-1 para finalizar): -1 
 A taxa total de km/litro foi 21.601423*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Inicio do código*/
+int main(void)
+{
+    /*Declaração de variáveis*/
+    float quilometragem_Dirigida = 0, litros_Abastecidos = 0;
+    float quilometragem_Por_Litro = 0, media_Quilometro_Por_Litro = 0;
+    int contador_Abastecimentos = 0;
+    float total_Abastecimentos = 0;
+
+    /*Entrada de dados*/
+    printf("Entre com os litros consumidos (-1 para finalizar): ");
+    scanf("%f", &litros_Abastecidos);
+
+    /*Bloco de comandos*/
+    while(litros_Abastecidos != -1)
+    {
+        contador_Abastecimentos++;
+        printf("Entre com os km percorridos: ");
+        scanf("%f", &quilometragem_Dirigida);
+
+        quilometragem_Por_Litro = quilometragem_Dirigida / litros_Abastecidos;      //Calculo
+
+        printf("A taxa total de km/litro: %.2f km/l\n\n", quilometragem_Por_Litro);
+        total_Abastecimentos += quilometragem_Por_Litro;
+
+        printf("Entre com os litros consumidos (-1 para finalizar): ");
+        scanf("%f", &litros_Abastecidos);
+    }
+
+    media_Quilometro_Por_Litro = (float)total_Abastecimentos / contador_Abastecimentos;
+
+    /*Saída de dados*/
+    printf("O numero de abastecimentos e de %d, e a media deste numero foi de %.2f km/l\n\n", contador_Abastecimentos,media_Quilometro_Por_Litro);
+
+    system("pause");
+    return 0;
+}
+/*Fim do código*/
--- Linguagem C/C como programar_DEITEL_6ed/03 - Desenvolvimento da Programação Estruturada/3.18/deithel.c
+++ Linguagem C/C como programar_DEITEL_6ed/03 - Desenvolvimento da Programação Estruturada/3.18/deithel.c
@@ -4,3 +4,14 @@
 3. Total de itens cobrados do cliente no mês em questão 
 4. Total de créditos aplicados à conta do cliente no mês cm questão. 
 5. Limite de crédito permitido. O programa deve receber esses dados, calcular o novo saldo (= saldo devedor inicial + cobranças — créditos) e determinar se o novo saldo supera o limite de crédito do cliente. Para os clientes cujo limite de crédito foi excedido, o programa deve exibir o número da conta do cliente, o limite de crédito e a mensagem "Limite de Crédito Excedido".*/
+
+#include <stdio.h>
+#include <stdlib.h>
+/*Inicio do código*/
+int main(void)
+{
+    /*Declaração de variáveis*/
+    int NumConta;
+    float SaldoDevedor
+}
+/*Fim do código*/
