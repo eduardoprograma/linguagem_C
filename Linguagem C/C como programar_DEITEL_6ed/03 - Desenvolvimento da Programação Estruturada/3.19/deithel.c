51da67f21a3d83466a3bd16ad4b9d156acf9b6bc
EDUARDO FELIZARDO CANDIDO <eduardo.candido@fatec.sp.gov.br>
2018-12-09 13:14:44 -0200

    R01

--- Linguagem C/C como programar_DEITEL_6ed/03 - Desenvolvimento da Programação Estruturada/3.19/deithel.c
+++ Linguagem C/C como programar_DEITEL_6ed/03 - Desenvolvimento da Programação Estruturada/3.19/deithel.c
@@ -1,1 +1,27 @@
 /*Uma grande companhia química paga seus vendedores por comissão. Os vendedores recebem $200 por semana mais 9 por cento de suas vendas brutas naquela semana. Por exemplo, um vendedor que vender o equivalente a $5000 em produtos químicos em uma semana recebe $200 mais 9 por cento de $5000, ou um total de $650. Desenvolva um programa em C que receba as vendas brutas de cada vendedor na última semana, calcule seu salário e o exiba. Processe os dados de um vendedor de cada vez.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+/*Inicio do código*/
+int main(void)
+{
+    /*Declaração de variáveis*/
+    float porcentagem = 0.09, salarioFixo = 200.00;
+    float vendasSemana = 0.00, valorReceber = 0;
+
+    /*Entrada de dados*/
+    printf("Entre com o valor da venda da semana (-1 para encerrar): $ ");
+    scanf("%f", &vendasSemana);
+    while(vendasSemana != -1)
+    {
+        valorReceber = salarioFixo + (vendasSemana * porcentagem);
+        printf("Vendas da semana: $ %.2f\n", vendasSemana);
+        printf("Salario fixo: $ %.2f\n", salarioFixo);
+        printf("Porcentagem das vendas: $ %.2f\n", vendasSemana * porcentagem);
+        printf("Valor a receber: $ %.2f\n\n", valorReceber);
+
+        printf("Entre com o valor da venda da semana (-1 para encerrar): $ ");
+        scanf("%f", &vendasSemana);
+    }
+}
+/*Fim do código*/
--- Linguagem C/C como programar_DEITEL_6ed/03 - Desenvolvimento da Programação Estruturada/3.20/deithel.c
+++ Linguagem C/C como programar_DEITEL_6ed/03 - Desenvolvimento da Programação Estruturada/3.20/deithel.c
@@ -1,1 +1,30 @@
 /*Os juros simples em um empréstimo são calculados pela fórmula juros = capital * taxa * dias / 365 A fórmula anterior admite que taxa c a taxa anual de juros e portanto inclui a divisão por 365 (dias). Desenvolva um programa em C que receba os valores de capital, taxa e dias de vários empréstimos, calcule os juros simples de cada empréstimo e os exiba, usando a fórmula anterior.*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/*Inicio do código*/
+int main(void)
+{
+    /*Declaração de varáveis*/
+    float juros = 0, capital = 0, taxa = 0, dias = 0;
+
+    /*Entrada de dados & Bloco de comando*/
+    printf("Entre com o valor do empréstimo (-1 para finalizar): ");
+    scanf("%f", &capital); 
+    while(capital != -1)
+    {
+        printf("Entre com a taxa de juros: ");
+        scanf("%f", &taxa); 
+        printf("Entre com o periodo do empréstimo em dias: ");
+        scanf("%f", &dias); 
+        juros = capital * taxa * dias / 365;
+        printf("O valor dos juros é: $%.2f\n\n", juros);
+
+        printf("Entre com o valor do empréstimo (-1 para finalizar): ");
+        scanf("%f", &capital);
+    }
+    printf("\n\n");
+    system("pause");
+    return 0;
+}
+/*Fim do código*/
