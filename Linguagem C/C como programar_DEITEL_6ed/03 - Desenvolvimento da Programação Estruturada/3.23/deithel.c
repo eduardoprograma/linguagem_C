8cd46fc1d557a67f183e4011243c582f46417b6f
EDUARDO FELIZARDO CANDIDO <eduardo.candido@fatec.sp.gov.br>
2018-12-09 14:28:54 -0200

    R01

--- Linguagem C/C como programar_DEITEL_6ed/03 - Desenvolvimento da Programação Estruturada/3.23/deithel.c
+++ Linguagem C/C como programar_DEITEL_6ed/03 - Desenvolvimento da Programação Estruturada/3.23/deithel.c
@@ -1,1 +1,21 @@
 /*Escreva um programa em C que utilize um loop para imprimir os números de 1 a 10, lado a lado na mesma linha e com 3 espaços entre eles.*/
+#include <stdio.h>
+#include <stdlib.h>
+/*Inicio do código*/
+int main(void)
+{
+    /*declare as variáveis*/
+    int x = 0;
+
+    /*Bloco de comandos*/
+    printf("Valor a se iniciar %d\n", x);
+    while(x < 10)
+    {
+        x++;
+        printf("%3d", x);
+    }
+    printf("\n\n");
+    system("pause");
+    return 0;
+}
+/*Fim do código*/
