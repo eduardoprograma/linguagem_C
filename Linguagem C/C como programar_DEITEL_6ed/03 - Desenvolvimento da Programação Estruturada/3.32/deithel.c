0e4b7961d43efd69b4552bc061431fa7c15e2b4f
EDUARDO FELIZARDO CANDIDO <eduardo.candido@fatec.sp.gov.br>
2018-12-10 16:33:33 -0200

    R01

--- Linguagem C/C como programar_DEITEL_6ed/03 - Desenvolvimento da Programação Estruturada/3.32/deithel.c
+++ Linguagem C/C como programar_DEITEL_6ed/03 - Desenvolvimento da Programação Estruturada/3.32/deithel.c
@@ -3,7 +3,7 @@ if (y == 8)
 if (x == 5) 
 printf("@@@@@\n"); t a else 
 printf("#####\n"),• printf("$$$$$\n"); printf("&&&&&\n");
-a) Admitindo x = 5ey = 8, a seguinte saída é produzida
+a) Admitindo x = 5 e y = 8, a seguinte saída é produzida
 @@@@@ 
 $ $ $ $ $ 
 & & & & &
@@ -16,3 +16,96 @@ d) Admitindo x = 5 e y = 8, a seguinte saída é produzida.Nota: As três últim
 # # # # # # 
 $ $ $ $ $
 & & & & &*/
+#include <stdio.h>
+#include <stdlib.h>
+/*Inicio do código*/
+int main(void)
+{
+    /*Declare as variaveis*/
+    int x = 5, y = 8;
+
+    /*Bloco de comando*/
+    if ( y == 8 ) 
+    {          
+        if ( x == 5 )
+        {
+            puts( "@@@@@" ); 
+        }
+        else 
+        {
+            puts( "#####" ); 
+            puts( "$$$$$" ); 
+            puts( "&&&&&" );
+        }
+    }
+puts("a) Admitindo x = 5 e y = 8, a seguinte saída é produzida"
+            /*@@@@@ 
+            $ $ $ $ $ 
+            & & & & &*/);
+    if ( y == 8 ) 
+    {          
+        if ( x == 5 )
+        {
+            puts( "@@@@@" );
+            puts( "$$$$$" ); 
+            puts( "&&&&&" ); 
+        }
+        else 
+        {
+            puts( "#####" ); 
+            puts( "$$$$$" ); 
+            puts( "&&&&&" );
+        }
+    }
+puts("b) Admitindo x = 5 e y = 8, a seguinte saída é produzida."
+            /*@@@@@*/);
+    if ( y == 8 ) 
+    {          
+        if ( x == 5 )
+        {
+            puts( "@@@@@" ); 
+        }
+        else 
+        {
+            puts( "#####" ); 
+            puts( "$$$$$" ); 
+            puts( "&&&&&" );
+        }
+    }
+puts("c) Admitindo x = 5 e y =8, a seguinte saída é produzida."
+            /*@@@@@ 
+            &&&&&*/);
+    if ( y == 8 ) 
+    {          
+        if ( x == 5 )
+        {
+            puts( "@@@@@" );
+            puts( "&&&&&" ); 
+        }
+        else 
+        {
+            puts( "#####" ); 
+            puts( "$$$$$" ); 
+            puts( "&&&&&" );
+        }
+    }
+puts("d) Admitindo x = 5 e y = 8, a seguinte saída é produzida.Nota: As três últimas instruções printf são parte de uma instrução composta."
+            /*# # # # # # 
+            $ $ $ $ $
+            & & & & &*/);
+    if ( y == 8 ) 
+    {          
+        if ( x == 5 )
+        {
+            puts( "#####" ); 
+            puts( "$$$$$" ); 
+            puts( "&&&&&" );
+        }
+        else 
+        {
+            puts( "#####" ); 
+            puts( "$$$$$" );
+        }
+    }        
+}
+/*Fim do código*/
