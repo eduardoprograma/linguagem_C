ab6fa88c22fb347b5a15a113335d9ec964ff01ca
EDUARDO FELIZARDO CANDIDO <eduardo.candido@fatec.sp.gov.br>
2018-12-12 23:07:16 -0200

    R01

---
+++ Linguagem C/C como programar_DEITEL_6ed/04 - Controle do Programa/4.04/C)Exercicio/deithel.c
@@ -0,0 +1,9 @@
+/*Encontre o erro em cada um dos segmentos de código a seguir e explique como corrigi-lo. 
+c)  switch (n) 
+        { 
+            case 1: puts( "The number is 1" );
+            case 2: puts( "The number is 2" ); 
+            break;
+            default: puts( "The number is not 1 or 2" ); 
+            break;
+        }*/
---
+++ Linguagem C/C como programar_DEITEL_6ed/04 - Controle do Programa/4.04/D)Exercicio/deithel.c
@@ -0,0 +1,9 @@
+/*Encontre o erro em cada um dos segmentos de código a seguir e explique como corrigi-lo. 
+a) x = 1; while ( x <= 10 ); ++x;
+} 
+b) for ( y = .1; y != 1.0; y += .1 ) printf( "%f\n", y );
+c) switch ( n ) { case 1: puts( "The number is 1" );
+case 2: puts( "The number is 2" ); break;
+default: puts( "The number is not 1 or 2" ); break;
+}
+d) O código a seguir deve imprimir valores de 1 a 10. n = 1; while ( n < 10 ) printf( "ud ", n++ );*/
