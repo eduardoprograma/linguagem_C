5af65d0fe5c37d41dc8a31be092870851f942faf
Eduardo Felizardo Cândido <eduardoprograma@users.noreply.github.com>
2019-01-17 11:31:47 -0200

    R01

--- Linguagem C/Linguagem C - Completa e descomplcada/6 - Vetores e matrIzes – arrays/10/lin.c
+++ Linguagem C/Linguagem C - Completa e descomplcada/6 - Vetores e matrIzes – arrays/10/lin.c
@@ -6,53 +6,58 @@
 int main(void)
 {
     /* declaração das variáveis */
-    int matriz[NUMERO][NUMERO] = {0};
-    int multiplos_7[NUMERO][NUMERO] = {0};
+    int matriz[NUMERO] = {0};
+    int multiplos_7[NUMERO] = {0};
     int i, j;
-    int multiplos_7i = 0, multiplos_7j = 0;
+    int multiplos_7j = 0;
 
     /* bloco de comandos */
-    for(i = 0; i <= 99; i++)
+    for(i = 0, j = 1; i <= 99; i++, j++)
     {
-        for(j = 0; j <= 99; j++)
+        multiplos_7j = j % 7;
+        if(multiplos_7j != 0)
         {
-            multiplos_7i = i % 7;
-            multiplos_7j = j % 7;
-            if(multiplos_7i != 0 && multiplos_7j != 0)
-            {
-                matriz[i][j];
-            }
-            else
-            {
-                multiplos_7[i][j];
-            }
+            matriz[i] = j;
+        }
+        else
+        {
+            multiplos_7[i] = j;
         }
     }
 
     /* saída de dados */
-    for(i = 0; i <= 99; i++)
+    for(i = 0, j = 1, j; i <= 99; i++, j++)
     {
-        for(j = 0; j <= 99; j++)
+        if(matriz[i] != 0)
         {
-            printf("%d\t", matriz[i][j]);
-            if(i != 0 && (i % 10) == 0)
-            {
-                printf("\n");
-            }
+            printf("%.1d\t", matriz[i]);
+        }
+        if(matriz[i] == 0)
+        {
+            printf("MM\t");
+        }
+        if(j % 10 == 0)
+        {
+            printf("\n");
         }
     }
+
     int n = 0;
-    printf("Se quiser saber quais sao os multiplos de 7, digite 1\nSe nao digite 2: ");
-    scanf("%d", n);
+    printf("\nSe quiser saber quais sao os multiplos de 7, digite 1\nSe nao digite 2: ");
+    scanf("%d", &n);
     if(n == 1)
     {
-        for(i = 0; i <= 99; i++)
+        for(i = 0, j = 1; i <= 99; i++, j++)
         {
-            for(j = 0; j <= 99; j++)
+            if(matriz[i] != 0)
             {
-                printf("%d\t", multiplos_7[i][j]);
+                printf("%s\t", "NM");
             }
-            if(i != 0 && (i % 10) == 0)
+            if(matriz[i] == 0)
+            {
+                printf("%d\t", multiplos_7[i]);
+            }
+            if(j % 10 == 0)
             {
                 printf("\n");
             }
