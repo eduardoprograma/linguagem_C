f9ce7c9b5b5e91efe6fde7ced9c750c026d10bbc
Eduardo Felizardo Cândido <eduardoprograma@users.noreply.github.com>
2019-02-09 07:38:30 -0200

    R00

---
+++ Linguagem C/Linguagem C - Completa e descomplcada/6 - Vetores e matrIzes – arrays/Matrizes/01/lin.c
@@ -0,0 +1,12 @@
+/* Faça um programa que leia uma matriz de tamanho 3 × 3. Imprima na tela o menor valor contido nessa matriz */
+#include <stdio.h>
+#include <stdlib.h>
+#define NUMERO 10
+/* inicio do código */
+int main()
+{
+    /* declaração de varipaveis */
+    int matriz[NUMERO][NUMERO][NUMERO] = {0};
+    int i, j, k;
+}
+/* fim do código */
