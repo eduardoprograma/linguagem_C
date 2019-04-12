3b8c86c4a6839ae1636533d63326871efedaf6ee
Eduardo Felizardo Cândido <41874842+eduardoprograma@users.noreply.github.com>
2019-04-12 11:05:35 -0300

    R00

--- Linguagem C/Linguagem C - Completa e descomplcada/8 - Tipos defInIdos peLo programador/04/lin.c
+++ Linguagem C/Linguagem C - Completa e descomplcada/8 - Tipos defInIdos peLo programador/04/lin.c
@@ -1,1 +1,18 @@
 /* Crie uma estrutura chamada Retângulo. Essa estrutura deverá conter o ponto superior esquerdo e o ponto inferior direito do retângulo. Cada ponto é definido por uma estrutura Ponto, a qual contém as posições X e Y. Faça um programa que declare e leia uma estrutura Retângulo e exiba a área e o comprimento da diagonal e o perímetro desse retângulo. */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct retangulo{
+    float x;
+    float y;
+    float area;
+    float comprimento;
+    float perimetro;
+};
+
+int main()
+{
+    struct ret;
+    
+}
