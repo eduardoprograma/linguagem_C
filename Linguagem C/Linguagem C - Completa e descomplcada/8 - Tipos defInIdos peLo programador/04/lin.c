703deb51995e782cec595b6a14c496c169c9afeb
Eduardo Felizardo Cândido <41874842+eduardoprograma@users.noreply.github.com>
2019-04-18 16:12:07 -0300

    R01

--- Linguagem C/Linguagem C - Completa e descomplcada/8 - Tipos defInIdos peLo programador/04/lin.c
+++ Linguagem C/Linguagem C - Completa e descomplcada/8 - Tipos defInIdos peLo programador/04/lin.c
@@ -2,17 +2,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+
+/* Inicio do código */
 
 struct retangulo{
     float x;
     float y;
     float area;
-    float comprimento;
+    float comprimento_Diagonal;
     float perimetro;
-};
+}retang;
 
 int main()
 {
-    struct ret;
-    
+    /* Declaração de varíaveis */
+    int i, j;
+
+    /* Entrada de dados */
+    printf("Digite o comprimento do retangulo: ");
+    scanf("%f", &retang.x);
+    printf("Digite a largura do retangulo: ");
+    scanf("%f", &retang.y);
+    printf("Muito bem!!!\nAperte enter para prosseguir\n");
+    system("pause");
+
+    /* Calculos */
+    retang.area = retang.x * retang.y;
+    retang.comprimento_Diagonal = sqrt((pow(retang.x, 2) + pow(retang.y, 2)));
+    retang.perimetro = 2 * (retang.x + retang.y);
+
+    /* Saída de dados */
+    for (i = 0; i < retang.y; i++)
+    {
+        for(j = 0;j < retang.x; j++)
+        {
+            printf(". ");
+        }
+        printf("\n");
+    }
+    printf("\n\n");
+    printf("Area = \n\n", retang.area);
+    printf("Comprimento Diagonal = \n\n", retang.comprimento_Diagonal);
+    printf("Perimentro = \n", retang.perimetro);
+
+    return 0;
 }
+
+/* Fim do código */
