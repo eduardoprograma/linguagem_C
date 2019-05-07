8dad57874a093e9809e0743685a4a60be116042a
Eduardo Felizardo Cândido <41874842+eduardoprograma@users.noreply.github.com>
2019-05-07 18:23:42 -0300

    R00

--- Linguagem C/Linguagem C - Completa e descomplcada/8 - Tipos defInIdos peLo programador/08/lin.c
+++ Linguagem C/Linguagem C - Completa e descomplcada/8 - Tipos defInIdos peLo programador/08/lin.c
@@ -1,6 +1,7 @@
 /* Crie uma estrutura capaz de armazenar o nome e a data de nascimento de uma pessoa. Agora, escreva um programa que leia os dados de seis pessoas. Calcule e exiba os nomes da pessoa mais nova e da mais velha. */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 /* Estrutura de dados */
@@ -10,6 +11,7 @@ struct Controle_pessoa
     int dia_nacimento;
     int mes_nacimento;
     int ano_nacimento;
+    int idade;
 }pessoa[6];
 /*Fim da estrutura de dados */
 
@@ -32,15 +34,12 @@ int main()
     /**********************************************************/
 
     /* declaração das varáveis */
-    int idade[6] = {0}, mes[6] = {0}, dia[6] = {0};
-    int i, j, k;
+    int i, j;
 
-    /* Controle da idade */
-    int anos[6] = {0}, meses[6] = {0}, dias[6] = {0};
-
-    /* Comparação da idade */
-    int idadeMaior = 0, diaMaior = 0, mesMaior = 0;
-    int idadeMenor = 0, diaMenor = 0, mesMenor = 0;
+    /* Varáveis de Comparação da idade */
+    int idadeMaior = 0, diaMaior = 0, mesMaior = 0, anoMaior = 0;
+    int idadeMenor = 0, diaMenor = 0, mesMenor = 0, anoMenor = 0;
+    char nomeMaior[60], nomeMenor[60];
 
 
     
@@ -57,74 +56,8 @@ int main()
         scanf("%d/%d/%d", &pessoa[i].dia_nacimento, &pessoa[i].mes_nacimento, &pessoa[i].ano_nacimento);
         //guarda a data.
         printf("\n\n");
+        pessoa[i].idade = (data_hora_atual -> tm_year + 1900) - pessoa[i].ano_nacimento;   //Calculo da idade
     }
-    
-    /* calculos de idades */
-    for(i = 0; i < 6; i++)
-    {
-        /* Mês de Nascimento == Mês Atual */
-        if(pessoa[i].mes_nacimento == (data_hora_atual ->tm_mon))
-        {
-            /* Dia de Nascimento > Dia Atual */
-            if(pessoa[i].dia_nacimento > (data_hora_atual ->tm_mday))
-            {
-
-            }
-            /* Dia de Nascimento < Dia Atual */
-            else if(pessoa[i].dia_nacimento < (data_hora_atual ->tm_mday))
-            {
-
-            }
-            /* Dia de NAscimento == Dia Atual */
-            else
-            {
-
-            }
-        }
-        /* Mês de Nascimento != Mês Atual */
-        else 
-        {
-            /* Mês Nascimento > Mês Atual */
-            if(pessoa[i].mes_nacimento > (data_hora_atual ->tm_mon))
-            {
-               /* Dia de Nascimento > Dia Atual */
-                if(pessoa[i].dia_nacimento > (data_hora_atual ->tm_mday))
-                {
-
-                }
-                /* Dia de Nascimento < Dia Atual */
-                else if(pessoa[i].dia_nacimento < (data_hora_atual ->tm_mday))
-                {
-
-                }
-                /* Dia de Nascimento == Dia Atual */
-                else
-                {
-
-                }
-            }
-            /* Mês Nascimento < Mês Atual */
-            else
-            {
-                /* Dia de Nascimento > Dia Atual */
-                if(pessoa[i].dia_nacimento > (data_hora_atual ->tm_mday))
-                {
-
-                }
-                /* Dia de Nascimento < Dia Atual */
-                else if(pessoa[i].dia_nacimento < (data_hora_atual ->tm_mday))
-                {
-
-                }
-                /* Dia de Nascimento == Dia Atual */
-                else
-                {
-
-                }
-            }
-        }
-    }
-    /* Fim do \Claculo da idade */
 
     /* Comparação de idades */
     
@@ -133,23 +66,85 @@ int main()
         /* Atribuição de idade menor */
         for(j = 0; j < 1; j++)
         {
-            idadeMenor = idade[i];
-            mesMenor = mes[i];
-            diaMenor = dia[i];
+            idadeMenor = pessoa[i].idade;
+            anoMenor = pessoa[i].ano_nacimento;
+            mesMenor = pessoa[i].mes_nacimento;
+            diaMenor = pessoa[i].dia_nacimento;
+            strcpy(nomeMenor, pessoa[i].nome);
         }
 
         /* Comparando idade Maior */
-        if(idade[i] > idadeMaior)
+        if(pessoa[i].idade > idadeMaior)
         {
-            if()
+            idadeMaior = pessoa[i].idade;
+            anoMaior = pessoa[i].ano_nacimento;
+            mesMaior = pessoa[i].mes_nacimento;
+            diaMaior = pessoa[i].dia_nacimento;
+            strcpy(nomeMaior,pessoa[i].nome);
+        }            
+
+        /* Idade Igual, porem mes e dia diferentes */
+        else if(pessoa[i].idade == idadeMaior)
+        {
+            if(pessoa[i].mes_nacimento > mesMaior)
+            {
+                idadeMaior = pessoa[i].idade;
+                anoMaior = pessoa[i].ano_nacimento;
+                mesMaior = pessoa[i].mes_nacimento;
+                diaMaior = pessoa[i].dia_nacimento;
+                strcpy(nomeMaior, pessoa[i].nome);
+            }
+            else if(pessoa[i].mes_nacimento == mesMaior)
+            {
+                if(pessoa[i].dia_nacimento > diaMaior)
+                {
+                    idadeMaior = pessoa[i].idade;
+                    anoMaior = pessoa[i].ano_nacimento;
+                    mesMaior = pessoa[i].mes_nacimento;
+                    diaMaior = pessoa[i].dia_nacimento;
+                    strcpy(nomeMaior, pessoa[i].nome);
+                }
+            }
         }
+        else
+        {
+            if(pessoa[i].idade < idadeMenor)
+            {
+                idadeMenor = pessoa[i].idade;
+                anoMenor = pessoa[i].ano_nacimento;
+                mesMenor = pessoa[i].mes_nacimento;
+                diaMenor = pessoa[i].dia_nacimento;
+                strcpy(nomeMenor, pessoa[i].nome);
+            }
+            else if(pessoa[i].idade == idadeMenor)
+            {
+                if(pessoa[i].mes_nacimento < mesMenor)
+                {
+                    idadeMenor = pessoa[i].idade;
+                    anoMenor = pessoa[i].ano_nacimento;
+                    mesMenor = pessoa[i].mes_nacimento;
+                    diaMenor = pessoa[i].dia_nacimento;
+                    strcpy(nomeMenor, pessoa[i].nome);
+                }
+                else if(pessoa[i].mes_nacimento == mesMenor)
+                {
+                    if(pessoa[i].dia_nacimento < diaMenor)
+                    {
+                        idadeMenor = pessoa[i].idade;
+                        anoMenor = pessoa[i].ano_nacimento;
+                        mesMenor = pessoa[i].mes_nacimento;
+                        diaMenor = pessoa[i].dia_nacimento;
+                        strcpy(nomeMenor, pessoa[i].nome);
+                    }
+                }
+            }
+        } 
     }
     
-    for(i = 0; i < 6; i++)
-    {
-        printf("Nome: %s\n", pessoa[i].nome);
-        printf("Idade: %d anos, %d meses, %d dias.\n\n", anos[i], meses[i], dias[i]);
-    }
+    /* Saída de dados */
+    printf("\n\n");
+    printf("%s e a pessoa mais velha, nascida em %d/%d/%d. Sua idade e %d\n\n", nomeMaior, diaMaior, mesMaior, anoMaior, idadeMaior);
+    printf("%s e a pessoa mais nova, nascida em %d/%d/%d. Sua idade e %d\n\n", nomeMenor, diaMenor, mesMenor, anoMenor, idadeMenor);
     return 0;
 }
 /* fim do código */
