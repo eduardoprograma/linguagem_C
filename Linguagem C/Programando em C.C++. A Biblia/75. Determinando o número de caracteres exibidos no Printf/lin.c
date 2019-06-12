c44cf6e2fc080fc2277b2204f1373c380ca1c2e3
Eduardo Felizardo Cândido <41874842+eduardoprograma@users.noreply.github.com>
2019-06-12 10:53:26 -0300

    R00_12/06/2019

--- Linguagem C/Programando em C.C++. A Biblia/75. Determinando o número de caracteres exibidos no Printf/lin.c
+++ Linguagem C/Programando em C.C++. A Biblia/75. Determinando o número de caracteres exibidos no Printf/lin.c
@@ -12,21 +12,21 @@ int main(void)
     int primeiro_conta;
     int segundo_conta;
 
-    printf("Bíblia%n do Programador C/C++, do Jamsal%n\n", &primeiro_conta, &segundo_conta);    //Não funciona...
+    printf("Bíblia%n do Programador C/C++, do Jamsal!%n\n", &primeiro_conta, &segundo_conta);    //Não funciona...
     printf("Primeiro conta %d Segundo conta %d\n", primeiro_conta, segundo_conta);              //Não funciona...
     printf("\n\n");
 
     //Forma atual de se fazer a contagem de caracteres utlizando o exemplo do livro....
-    
+
     int tamanho1;
     int tamanho2;
     char palavra[7] = "Biblia";
-    char frase[40] = "do Programador C/C++, do Jamsal";
+    char frase[40] = "do Programador C/C++, do Jamsal!";
 
     tamanho1 = strlen(palavra);
     tamanho2 = strlen(frase);
 
     printf("%s %s\n", palavra, frase);
-    printf("Primeiro conta %d Segundo conta %d\n", tamanho1, tamanho2);
+    printf("Primeiro conta %d Segundo conta %d\n", tamanho1, tamanho1 + tamanho2);
 }
 
