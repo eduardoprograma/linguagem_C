1a9be91fcd2407d5638be8ce1c7585f92765ff42
Eduardo Felizardo Cândido <41874842+eduardoprograma@users.noreply.github.com>
2019-08-03 10:17:57 -0300

    03/08/2019_R00

---
+++ Linguagem C/Exercicios/6.23/gt.c
@@ -0,0 +1,69 @@
+/*
+ ============================================================================
+ Name			: gt.c
+ Author      	: Eduardo Felizardo Cândido
+ Creation date 	: 03/08/2019
+ Version     	: R00
+ Copyright   	: programacaoedu@gmail.com
+ Description 	: Gráficos de tartaruga.
+ ============================================================================
+*/
+/*
+ ============================================================================
+6.23 	Gráficos de tartaruga. 
+	A linguagem Logo, que é particularmente popular entre os usuários de com-
+	putadores pessoais, tornou famoso o conceito de gráficos de tartaruga. I-
+	magine uma tartaruga mecânica que caminhe pela sala sob o controle de um 
+	programa em C. A tartaruga segura uma caneta em uma de duas posições, pa-
+	ra cima ou para baixo. Quando a caneta está para baixo, a tartaruga traça 
+	as formas enquanto se movimenta; quando a caneta está para cima, a tarta-
+	ruga se movimenta livremente, sem escrever nada. Neste problema, você si-
+	mulará a operação da tartaruga e também criará um bloco de rascunho comp-
+	utadorizado. Use um array de 50 por 50 floor que seja inicializado em ze-
+	ros. Leia comandos de um array em que eles estejam contidos. Registre a 
+	posição atual da tartaruga o tempo todo, e se a caneta está para cima ou 
+	para baixo. Suponha que a tartaruga sempre comece na posição 0,0 do piso 
+	com sua caneta para cima. O conjunto de comandos da tartaruga que seu pr-
+	ograma deverá processar aparece na Figura 6.24. Suponha que a tartaruga 
+	esteja em algum ponto próximo do centro do piso. O ‘programa’ a seguir 
+	desenharia e imprimiria um quadrado de 12 por 12: 
+						2
+						5.12 
+						3
+						5.12 
+						3
+						5.12 
+						3
+						5.12 
+						1 
+						6 
+						9
+	Enquanto a tartaruga se mover com a caneta para baixo, defina os elemen-
+	tos apropriados do array floor como 1. Quando o comando 6 (imprimir) for 
+	dado, onde houver um 1 no array, mostre um asterisco ou algum outro car-
+	actere à sua escolha. Sempre que houver um zero, mostre um espaço em 
+	branco. Escreva um programa que implemente as capacidades dos gráficos 
+	de tartaruga discutidas aqui. Escreva vários programas de gráficos de 
+	tartaruga para desenhar formas interessantes. Acrescente outros comandos 
+	para aumentar o poder de sua linguagem de gráficos de tartaruga.
+	
+				Comando 				Significado
+					1 					Caneta para cima 
+					2 					Caneta para baixo
+					3 					Virar à direita 
+					4					Virar à esquerda
+					5.10				Move 10 espaços adiante (ou algum nú-
+					mero diferente de 10)
+					6 					Imprime o array de 50 por 50 array 
+					9					Fim dos dados (sentinela)
+ ============================================================================
+*/
+#include <stdio.h>
+
+int main(void)		//inicio da função main.
+{
+	/* declaração das varáveis */
+	/* entrada de dados */
+	/* processamento */
+	/* saída de dados */
+}			//fim da função main.
