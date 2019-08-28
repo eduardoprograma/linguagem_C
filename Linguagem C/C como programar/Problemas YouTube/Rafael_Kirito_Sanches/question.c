aac0df1f491dc3aac0422ef3f0f13fe936adff01
Eduardo Felizardo Cândido <41874842+eduardoprograma@users.noreply.github.com>
2019-08-28 19:24:27 -0300

    R00

---
+++ Linguagem C/C como programar/Problemas YouTube/Rafael_Kirito_Sanches/question.c
@@ -0,0 +1,10 @@
+/*
+===========================================================
+Comentário em destaque
+Rafael_kirito Sanchez
+===========================================================
+*/
+#include <stdio.h>
+
+/* inicio da função main */
+/* fim da função main */
